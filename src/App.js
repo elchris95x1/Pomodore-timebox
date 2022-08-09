e70b5ac615fac9e4c4bf3d56b7354310751d84d8
Chris <achris95x1@gmail.com>
2022-08-09 14:22:34 -0500

    First commit
    This project is migrated from a full stack project that was using .net framework to use react only

--- src/App.js
+++ src/App.js
@@ -1,24 +1,201 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import Pomodoro from "./components/Pomodoro"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div class="container shadow-lg p-3 mb-5 bg-white rounded col-10 justify-content-center">
+
+        <Pomodoro />
+
+        <Formik
+          initialValues={{ goal1: '', goal2: '', goal3: '' }}
+          validate={values => {
+            //validation goes here
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {({ isSubmitting }) => (
+            <Form>
+
+              <div class="container " >
+                <div class="row form-group pb-5">
+                  <div class="col" >
+                    <label class="form-label">Name</label>
+                    <Field type="text" name="fullName" class="form-control" autoomplete="off" />
+                  </div>
+                  <div class="col" >
+                    <label class="form-label">Date</label>
+                    <Field type="date" name="date" class="form-control" autocomplete="off" />
+                  </div>
+                </div>
+
+                <div class="row form-group">
+                  <div class="col">
+                    <div>
+                      <label class="form-label">Top Priorities</label>
+                      <Field type="text" name="goal1" class="form-control mb-1" autocomplete="off" />
+                      <Field type="text" name="goal2" class="form-control mb-1" autocomplete="off" />
+                      <Field type="text" name="goal3" class="form-control mb-1" autocomplete="off" />
+                    </div>
+
+                    <div class="pt-2">
+                      <label class="form-label">Braindump</label>
+                      <Field as="textarea" name="braindump" class="form-control mb-1" rows="15" />
+                    </div>
+                  </div>
+
+
+                  <div class="col form-group">
+                    <div class="row ">
+                      <div class=" col-1">
+                        <label class="form-label">Hrs</label>
+                      </div>
+                      <div class=" col ">
+                        <label class="form-label">:00</label>
+                      </div>
+                      <div class="col ">
+                        <label class="form-label">:30</label>
+                      </div>
+                    </div>
+
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">06</span>
+                        </div>
+                        <Field type="text" name="timebox1" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox2" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">07</span>
+                        </div>
+                        <Field type="text" name="timebox3" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox4" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">08</span>
+                        </div>
+                        <Field type="text" name="timebox5" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox6" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">09</span>
+                        </div>
+                        <Field type="text" name="timebox7" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox8" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">10</span>
+                        </div>
+                        <Field type="text" name="timebox9" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox10" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">11</span>
+                        </div>
+                        <Field type="text" name="timebox11" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox12" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">12</span>
+                        </div>
+                        <Field type="text" name="timebox13" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox14" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">01</span>
+                        </div>
+                        <Field type="text" name="timebox15" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox16" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">02</span>
+                        </div>
+                        <Field type="text" name="timebox17" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox18" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">03</span>
+                        </div>
+                        <Field type="text" name="timebox19" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox20" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">04</span>
+                        </div>
+                        <Field type="text" name="timebox21" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox22" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">05</span>
+                        </div>
+                        <Field type="text" name="timebox23" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox24" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+                    <div class="row ">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="">06</span>
+                        </div>
+                        <Field type="text" name="timebox25" class="form-control mb-1" autocomplete="off" />
+                        <Field type="text" name="timebox26" class="form-control mb-1" autocomplete="off" />
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+
+              {/* <button type="submit" disabled={isSubmitting} class="btn, btn-primary">
+                Submit
+              </button> */}
+            </Form>
+          )}
+        </Formik>
+
+
+      </div >
   );
 }
 
---
+++ src/components/Pomodoro.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from "react"
+
+
+
+export default function Pomodoro() {
+    const [minutes, setMinutes] = useState(25)
+    const [seconds, setSeconds] = useState(0)
+    const [message, setMessage] = useState("Time to focus!")
+    const [bgColor, setBgColor] = useState("pomodoro-mode")
+    const [startTimer, setStartTimer] = useState(false)
+    const [pomodoroCounter, setPomodoroCounter] = useState(1)
+    const [buttonText, setButtonText] = useState('Start')
+    // const [mode, setmode] = useState()
+
+    let interval;
+    useEffect(() => {
+        interval = setInterval(() => {
+            clearInterval(interval)
+            if (startTimer) {
+                if (seconds === 0) {
+                    setSeconds(59)
+                    setMinutes(minutes - 1)
+                }
+                else {
+                    setSeconds(seconds - 1)
+                }
+            }
+            autoSwitchMode()
+        }, 1000)
+        return () => clearInterval(interval);
+    })
+
+    function handleStartStop() {
+        setStartTimer(prevStartTimer => !prevStartTimer)
+        setButtonText(() => startTimer ? "Start" : "Pause");
+    }
+
+    function timerMode(modeString) {
+        if (modeString === "pomodoro") {
+            setBgColor("pomodoro-mode")
+            setMinutes(25)
+            setSeconds(0)
+            setMessage("Time to focus!")
+        } else if (modeString === "shortBreak") {
+            setBgColor("short-break-mode")
+            setMinutes(5)
+            setSeconds(0)
+            setMessage("Take a short break!")
+        }
+        else if (modeString === "longBreak") {
+            setBgColor("long-break-mode")
+            setMinutes(15)
+            setSeconds(0)
+            setMessage("Take a long break!")
+        }
+        else {
+            console.log("Error: Timer Mode Function");
+        }
+    }
+
+    function handleTimerTab(event) {
+        if (event.target.value === "pomodoro") {
+            timerMode("pomodoro")
+        } else if (event.target.value === "shortBreak") {
+            timerMode("shortBreak")
+        }
+        else if (event.target.value === "longBreak") {
+            timerMode("longBreak")
+        }
+        else {
+            console.log("Error: Radio buttons");
+        }
+    }
+
+    function autoSwitchMode() {
+        if (!seconds && !minutes) {
+            if (bgColor === "pomodoro-mode") {
+                if (pomodoroCounter === 4) {
+                    timerMode("longBreak")
+                    setPomodoroCounter(0)
+                }
+                else {
+                    timerMode("shortBreak")
+                    setPomodoroCounter(prevPomodoroCounter => prevPomodoroCounter + 1)
+                }
+            } else {
+                timerMode("pomodoro")
+            }
+        }
+    }
+
+    const timerMinutes = minutes < 10 ? `0${minutes}` : minutes
+    const timerSeconds = seconds < 10 ? `0${seconds}` : seconds
+
+    return (
+        <div className={bgColor}>
+            <div class="col text-center shadow-sm p-3 mb-5 text-white rounded" >
+                <h2>Pomodore Timebox</h2>
+                <div>
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons" >
+                        <label class="btn btn-outline-light">
+                            <input type="radio" name="options" id="option1" value="pomodoro" autocomplete="off" onClick={handleTimerTab} /> Pomodoro
+                        </label>
+                        <label class="btn btn-outline-light">
+                            <input type="radio" name="options" id="option2" value="shortBreak" autocomplete="off" onClick={handleTimerTab} /> Short Break
+                        </label>
+                        <label class="btn btn-outline-light">
+                            <input type="radio" name="options" id="option3" value="longBreak" autocomplete="off" onClick={handleTimerTab} /> Long Break
+                        </label>
+                    </div>
+
+                    <div class="display-1">
+                        {timerMinutes}:{timerSeconds}
+                    </div>
+                    <div class="display-6 p-2">
+                        {message}
+                    </div>
+
+                    <button type="button" class="btn btn-primary btn-lg " onClick={handleStartStop}>{buttonText}</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
